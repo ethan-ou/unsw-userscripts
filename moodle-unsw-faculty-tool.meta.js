5aebc7457aab6781d8f9946c0c075ed70bd7caaf
Ethan Ou <45083970+ethan-ou@users.noreply.github.com>
2025-07-03 12:26:29 +1000

    Update urls

--- moodle-unsw-faculty-tool.meta.js
+++ moodle-unsw-faculty-tool.meta.js
@@ -1,13 +1,15 @@
 // ==UserScript==
-// @name       moodle-unsw-faculty-tool
-// @namespace  https://github.com/ethan-ou
-// @version    0.3.0
-// @author     Ethan Ou
-// @match      https://moodle.telt.unsw.edu.au/*
-// @match      https://unsw-mdl-stage.enter.openlms.cloud/*
-// @match      https://ecos.unsw.edu.au/*
-// @grant      GM.deleteValue
-// @grant      GM.getValue
-// @grant      GM.setValue
-// @grant      GM_addStyle
+// @name         moodle-unsw-faculty-tool
+// @namespace    https://github.com/ethan-ou
+// @version      0.3.0
+// @author       Ethan Ou
+// @downloadURL  https://raw.githubusercontent.com/ethan-ou/unsw-userscripts/main/moodle-unsw-faculty-tool.user.js
+// @updateURL    https://raw.githubusercontent.com/ethan-ou/unsw-userscripts/main/moodle-unsw-faculty-tool.meta.js
+// @match        https://moodle.telt.unsw.edu.au/*
+// @match        https://unsw-mdl-stage.enter.openlms.cloud/*
+// @match        https://ecos.unsw.edu.au/*
+// @grant        GM.deleteValue
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM_addStyle
 // ==/UserScript==
