581f8c1a0e37768f74cee25819f5a2e7d92f5e86
Ethan Ou <45083970+ethan-ou@users.noreply.github.com>
2025-07-03 12:23:00 +1000

    moodle-unsw-faculty-tool v0.3.0

---
+++ moodle-unsw-faculty-tool.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name       moodle-unsw-faculty-tool
+// @namespace  https://github.com/ethan-ou
+// @version    0.3.0
+// @author     Ethan Ou
+// @match      https://moodle.telt.unsw.edu.au/*
+// @match      https://unsw-mdl-stage.enter.openlms.cloud/*
+// @match      https://ecos.unsw.edu.au/*
+// @grant      GM.deleteValue
+// @grant      GM.getValue
+// @grant      GM.setValue
+// @grant      GM_addStyle
+// ==/UserScript==
