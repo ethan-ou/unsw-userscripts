96fc4fd45eaaec095feab9551a86812505d039fd
Ethan Ou <45083970+ethan-ou@users.noreply.github.com>
2025-07-24 15:11:29 +1000

    Version 0.3.3

--- moodle-unsw-faculty-tool.meta.js
+++ moodle-unsw-faculty-tool.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Moodle UNSW Faculty Tool
 // @namespace    https://github.com/ethan-ou
-// @version      0.3.2
+// @version      0.3.3
 // @author       Ethan Ou
 // @downloadURL  https://raw.githubusercontent.com/ethan-ou/unsw-userscripts/main/moodle-unsw-faculty-tool.user.js
 // @updateURL    https://raw.githubusercontent.com/ethan-ou/unsw-userscripts/main/moodle-unsw-faculty-tool.meta.js
